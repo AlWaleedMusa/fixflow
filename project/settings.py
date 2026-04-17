cf7d57a34fb6a561c74f918fe43d692fc6ad9859
AlWaleed Musa <jako_249@gmail.com>
2026-04-17 21:35:27 +0200

    added some enviroment files and removed the mockup html

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-as*8*-arxq-ir(&&4_ya!m6aleb)4q4p9py)gc-6^jv@ecq4cf"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -125,5 +129,5 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-PAYHIP_URL = "https://payhip.com/b/oNnh3"
-FREE_TOOL_URL = "https://payhip.com/b/xIJQO"
+PAYHIP_URL = os.getenv("PAYHIP_URL")
+FREE_TOOL_URL = os.getenv("FREE_TOOL_URL")
